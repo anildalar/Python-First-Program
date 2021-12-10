70e9e5880512fb048c20b508c5f99f2d4d5ce359
“anildalar” <dollorinfotech@gmail.com>
2021-12-10 10:54:42 +0530

    new

---
+++ first.py
@@ -0,0 +1,10 @@
+
+#.1 Function Defination
+def hello(name): # name is a formal Arguemtn
+    print("Hello "+name+" We Everyone we are learning Python")
+
+
+#2. Function Calling
+hello("Poonum")
+hello("Manish")
+hello("Deepak")
